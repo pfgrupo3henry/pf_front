17dc9b8a5d947bdafdbe7c59a2dae6dfc6425e3b
Tobias Blaksley Iraola <tblanksley@beru.io>
2023-03-21 13:25:16 -0300

    crear juego

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import {Nav} from "./Components/Nav/nav"
-import {CardElement} from "./Components/Card/card"
+import { Nav } from "./Components/Nav/nav"
+import { CardElement } from "./Components/Card/card"
 import { CardList } from './Components/CardList/cardList';
 import { PaginationHome } from './Components/Pagination/pagination';
 import CardDetail from './Components/CardDetail/CardDetail';
 import Footer from './Components/Footer/Footer';
+import CrearJuego from "./Components/CrearJuego/CrearJuego";
 
 
 
@@ -12,22 +13,27 @@ import Footer from './Components/Footer/Footer';
 function App() {
   return (
 
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route  path="/" element={<Nav/>} />
-        </Routes>
 
-        <Routes>
-          <Route  path="/" element={<CardList/>} />
-        </Routes>
-        
-        <Routes>
-          <Route exact path="/game/:id" element={<CardDetail />} />
-        </Routes>
-        
-         <Footer />    
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+
+      <Nav />
+
+      <Routes>
+
+        <Route path="/" element={<CardList />} />
+
+        <Route path="/admin" element={<CrearJuego />} />
+
+        <Route exact path="/game/:id" element={<CardDetail />} />
+
+      </Routes>
+
+      <Footer />
+
+    </BrowserRouter>
+
+  );
+
+};
 
 export default App;
