021f80346ada25a2e9712930ad6384604b6c7fc5
Tobias Blaksley Iraola <tblanksley@beru.io>
2023-03-21 18:05:46 -0300

    crear juego

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
 import "./Admin.css";
-import { Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import {
+    Form,
+    Input,
+    Button,
+    Select,
+    DatePicker,
+    Upload,
+} from 'antd';
 
 
 
 function CrearJuego() {
+
+    const { RangePicker } = DatePicker;
+    const { TextArea } = Input;
 
     const [state, setState] = useState("buttons");
     const [inputJuego, setInputJuego] = useState({
@@ -90,131 +101,154 @@ function CrearJuego() {
 
     } else if (state === "crear-juego") {
 
-        const onChangeInputJuego = (e) => {
-            setInputJuego({
-                ...inputJuego,
-                [e.target.name]: e.target.value
-            });
+        if (true) {
 
-            console.log(inputJuego);
+            const onChangeInputJuego = (e) => {
+                setInputJuego({
+                    ...inputJuego,
+                    [e.target.name]: e.target.value
+                });
+
+                console.log(inputJuego);
+            };
+
+            const onChangeSelectGenero = (e) => {
+
+                const selectGeneros = document.getElementById('select-generos');
+
+
+                setInputJuego({
+                    ...inputJuego,
+                    [e.target.name]: `${inputJuego.genero} ${e.target.value}`
+                });
+
+                console.log(inputJuego);
+
+                selectGeneros.value = "Encontra tu genero";
+            };
+
+            const onChangeSelectConsola = (e) => {
+
+                const selectConsolas = document.getElementById('select-consolas');
+
+
+                setInputJuego({
+                    ...inputJuego,
+                    [e.target.name]: `${inputJuego.consola} ${e.target.value}`
+                });
+
+                console.log(inputJuego);
+
+                selectConsolas.value = "Encontra tu consola";
+            };
+
+            if (inputJuego.nombre.length >= 1 && inputJuego.nombre.length <= 3) {
+                var errorName = "error";
+            };
+
+            if (inputJuego.precio.length !== 0) {
+
+                if (isNaN(inputJuego.precio) || inputJuego.precio.length <= 2 || inputJuego.precio.length >= 5) {
+                    var errorPrecio = "error";
+                }
+
+            };
+
+            if (inputJuego.rating.length !== 0) {
+
+                if (isNaN(inputJuego.rating) || inputJuego.rating.length >= 2 || inputJuego.rating < 1
+                    || inputJuego.rating >= 6) {
+                    var errorRating = "error";
+                }
+
+            };
+
+            if (inputJuego.descripcion.length >= 1 && inputJuego.descripcion.length <= 9) {
+                var errorDescripcion = "error";
+            };
+
+            if (inputJuego.imagen.length >= 1 && inputJuego.imagen.length <= 9) {
+                var errorImagen = "error";
+            };
+
+            return (
+
+                <div className="form-ant">
+
+                    <Form
+                        labelCol={{ span: 4 }}
+                        wrapperCol={{ span: 14 }}
+                        layout="horizontal"
+                        style={{ maxWidth: 600 }}
+                    >
+
+                        <h1>Crear Juego</h1>
+
+                        <Form.Item label="Nombre">
+                            <Input onChange={onChangeInputJuego} name="nombre" placeholder="escriba el nombre" value={inputJuego.nombre} />
+                            {errorName === "error" ? <p className="parrafo-juego">Nombre debe tener minimo 3 letras</p> : <p></p>}
+                        </Form.Item>
+
+                        <Form.Item label="Precio">
+                            <Input onChange={onChangeInputJuego} name="precio" placeholder="escriba el precio" value={inputJuego.precio} />
+                            {errorPrecio === "error" ? <p className="parrafo-juego">Precio debe tener 3 o 4 numeros</p> : <p></p>}
+                        </Form.Item>
+
+                        <Form.Item label="Rating">
+                            <Input onChange={onChangeInputJuego} name="rating" placeholder="escriba el rating" value={inputJuego.rating} />
+                            {errorRating === "error" ? <p className="parrafo-juego">Rating debe ser mayor a 0 y menor a 6</p> : <p></p>}
+                        </Form.Item>
+
+                        <Form.Item label="Genero">
+                            <select name="genero" id="select-generos" onChange={onChangeSelectGenero} className="select-form">
+                                <option>Encontra tu genero</option>
+                                <option>Carreras</option>
+                                <option>Multijugador</option>
+                                <option>Pelas</option>
+                                <option>Aventuras</option>
+                            </select>
+                        </Form.Item>
+
+                        {inputJuego.genero ? <p className="parrafo-juego-select">{inputJuego.genero}</p> : <p></p>}
+
+                        <Form.Item label="Consola">
+                            <select name="consola" id="select-consolas" onChange={onChangeSelectConsola} className="select-form">
+                                <option>Encontra tu consola</option>
+                                <option>Play Station</option>
+                                <option>Sega</option>
+                                <option>Nintendo</option>
+                                <option>X-Box</option>
+                            </select>
+                        </Form.Item>
+
+                        {inputJuego.consola ? <p className="parrafo-juego-select">{inputJuego.consola}</p> : <p></p>}
+
+                        <Form.Item label="Descripcion">
+                            <TextArea rows={4} onChange={onChangeInputJuego} name="descripcion" placeholder="descripcion" value={inputJuego.descripcion} />
+                            {errorDescripcion === "error" ? <p className="parrafo-juego">Descripcion debe minimo 10 letras</p> : <p></p>}
+                        </Form.Item>
+
+                        <Form.Item label="Imagen" valuePropName="fileList">
+                            <Upload action="/upload.do" listType="picture-card">
+                                <div>
+                                    <PlusOutlined />
+                                    <div style={{ marginTop: 8 }}>Upload</div>
+                                </div>
+                            </Upload>
+                        </Form.Item>
+
+                        <Form.Item label="Buttons">
+                            <Button onClick={onClickReturn}>Submit</Button>
+                            <Button onClick={onClickReturn}>Return</Button>
+                        </Form.Item>
+
+                    </Form>
+
+                </div >
+
+            );
+
         };
-
-        const onChangeSelectGenero = (e) => {
-
-            const selectGeneros = document.getElementById('select-generos');
-
-
-            setInputJuego({
-                ...inputJuego,
-                [e.target.name]: `${inputJuego.genero} ${e.target.value}`
-            });
-
-            console.log(inputJuego);
-
-            selectGeneros.value = "Encontra tu genero";
-        };
-
-        const onChangeSelectConsola = (e) => {
-
-            const selectConsolas = document.getElementById('select-consolas');
-
-
-            setInputJuego({
-                ...inputJuego,
-                [e.target.name]: `${inputJuego.consola} ${e.target.value}`
-            });
-
-            console.log(inputJuego);
-
-            selectConsolas.value = "Encontra tu consola";
-        };
-
-        if (inputJuego.nombre.length >= 1 && inputJuego.nombre.length <= 3) {
-            var errorName = "error";
-        };
-
-        if (inputJuego.precio.length !== 0) {
-
-            if (isNaN(inputJuego.precio) || inputJuego.precio.length <= 2 || inputJuego.precio.length >= 5) {
-                var errorPrecio = "error";
-            }
-
-        };
-
-        if (inputJuego.rating.length !== 0) {
-
-            if (isNaN(inputJuego.rating) || inputJuego.rating.length >= 2 || inputJuego.rating < 1
-                || inputJuego.rating >= 6) {
-                var errorRating = "error";
-            }
-
-        };
-
-        if (inputJuego.descripcion.length >= 1 && inputJuego.descripcion.length <= 9) {
-            var errorDescripcion = "error";
-        };
-
-        if (inputJuego.imagen.length >= 1 && inputJuego.imagen.length <= 9) {
-            var errorImagen = "error";
-        };
-
-        return (
-
-            <div className="body-color-admin">
-
-                <section className="form-admin">
-
-                    <h4>Crear Juego</h4>
-
-                    <input onChange={onChangeInputJuego} className="controls" type="text" name="nombre" placeholder="escriba el nombre" value={inputJuego.nombre} />
-                    {errorName === "error" ? <p className="parrafo-juego">Nombre debe minimo 3 letras</p> : <p></p>}
-
-                    <input onChange={onChangeInputJuego} className="controls" type="text" name="precio" placeholder="escriba el precio" value={inputJuego.precio} />
-                    {errorPrecio === "error" ? <p className="parrafo-juego">Precio debe tener 3 o 4 numeros</p> : <p></p>}
-
-                    <input onChange={onChangeInputJuego} className="controls" type="text" name="rating" placeholder="escriba el rating" value={inputJuego.rating} />
-                    {errorRating === "error" ? <p className="parrafo-juego">Rating debe ser mayor a 0 y menor a 6</p> : <p></p>}
-
-                    <input onChange={onChangeInputJuego} className="controls" type="text" name="descripcion" placeholder="descripcion" value={inputJuego.descripcion} />
-                    {errorDescripcion === "error" ? <p className="parrafo-juego">Descripcion debe minimo 10 letras</p> : <p></p>}
-
-                    <input onChange={onChangeInputJuego} className="controls" type="text" name="imagen" placeholder="imagen" value={inputJuego.imagen} />
-                    {errorImagen === "error" ? <p className="parrafo-juego">Imagen debe minimo 10 letras</p> : <p></p>}
-
-                    <select className="controls" onChange={onChangeSelectGenero} name="genero" id="select-generos">
-
-                        <option>Encontra tu genero</option>
-                        <option>Accion</option>
-                        <option>Aventura</option>
-                        <option>Multijugador</option>
-                        <option>Carreras</option>
-
-                    </select>
-
-                    {inputJuego.genero ? <p className="parrafo-juego">{inputJuego.genero}</p> : <p></p>}
-
-                    <select className="controls" onChange={onChangeSelectConsola} name="consola" id="select-consolas">
-
-                        <option>Encontra tu consola</option>
-                        <option>PlayStation 1</option>
-                        <option>PlayStation 2</option>
-                        <option>PlayStation 3</option>
-                        <option>PlayStation 4</option>
-                        <option>PlayStation 5</option>
-
-                    </select>
-
-                    {inputJuego.consola ? <p className="parrafo-juego">{inputJuego.consola}</p> : <p></p>}
-
-                    <button className="buttons-form" onClick={onClickReturn}>Sumbit</button>
-
-                    <button className="buttons-return" onClick={onClickReturn}>Return</button>
-
-                </section>
-
-            </div>
-
-        )
 
     } else if (state === "modificar-juego") {
 
