e5fc1d58ff73ce93813f11c25a17aa599b5954f1
Tobias Blaksley Iraola <tblanksley@beru.io>
2023-03-21 21:46:46 -0300

    pull develop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import {Nav} from "./Components/Nav/nav"
-import {CardElement} from "./Components/Card/card"
+import { Nav } from "./Components/Nav/nav"
+import { CardElement } from "./Components/Card/card"
 import { Home } from './Components/Home/Home';
 import { PaginationHome } from './Components/Pagination/pagination';
 import { UserInfo } from './Components/UserInfo/userInfo';
@@ -18,11 +18,11 @@ function App() {
 
     <div className="App">
       <BrowserRouter>
-        <Nav/>
+        <Nav />
         <Routes>
-          <Route  path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
         </Routes>
-        
+
         <Routes>
           <Route exact path="/game" element={<CardDetail />} />
         </Routes>
@@ -34,12 +34,18 @@ function App() {
         <Routes>
           <Route exact path="/admin" element={<Admin />} />
         </Routes>
-        
 
-         <Footer />    
+
+        <Footer />
       </BrowserRouter>
     </div>
   );
 }
 
 export default App
+
+
+/*
+
+
+*/
