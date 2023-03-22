1936fe1486c173900c2e00e34dc06b8444b76571
Tobias Blaksley Iraola <tblanksley@beru.io>
2023-03-22 13:36:51 -0300

    app.js...profile

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Nav } from "./Components/Nav/nav"
 import { CardElement } from "./Components/Card/card"
 import { Home } from './Components/Home/Home';
 import { PaginationHome } from './Components/Pagination/pagination';
-import { UserInfo } from './Components/UserInfo/UserInfo';
+import { UserInfo } from './Components/UserInfo/userInfo';
 import CardDetail from './Components/CardDetail/CardDetail';
 import Footer from './Components/Footer/Footer';
 import Filter from './Components/FilterHome/filterHome'
