3b53304e0cecb8274f34e072d907898574013568
celinadelacruzriz <celinadelacruzriz@gmail.com>
2023-04-18 18:38:39 -0300

    chatbot

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { CardElement } from "../Card/card";
 import { Slider } from "../Slider/Slider";
-import { Menu, FloatButton, Pagination, Alert, Divider } from "antd";
+import { Menu, FloatButton, Pagination, Alert, Divider, Modal } from "antd";
 import "../FilterHome/filterHome.css";
 import "./Home.css";
 import "../Pagination/pagination.css";
@@ -15,7 +15,7 @@ import axios from "axios";
 import { SmileOutlined, WhatsAppOutlined } from "@ant-design/icons";
 import RatingWeb from "../RatingWeb/RatingWeb";
 import Chatty from "../Chatbot/Chatty";
-import ChatBubble from "../Chatbot/ChatMessage";
+import { RiChat1Line } from "react-icons/ri";
 
 function Home(label, key, icon, children, type) {
   const { logout } = useAuth0();
@@ -201,6 +201,15 @@ function Home(label, key, icon, children, type) {
     updateElementsToShow(1);
   }, [filteredVideogames]);
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   if (card) {
     return (
       <div className="home-component">
@@ -238,6 +247,18 @@ function Home(label, key, icon, children, type) {
                     icon={<SmileOutlined />}
                   />
                 </Link>
+                <Divider type="vertical" />
+                <FloatButton
+                  tooltip="Chatea con Henry!"
+                  onClick={showModal}
+                  icon={<RiChat1Line />}></FloatButton>
+                <Modal
+                  title="Chatbot"
+                  open={isModalVisible}
+                  onCancel={handleCancel}
+                  footer={null}>
+                  <Chatty />
+                </Modal>
               </FloatButton.Group>
             </>
           </div>
