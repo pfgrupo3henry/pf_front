f175e372503698ee91e84f54105ff089de7d77ca
Molli1992 <99288359+Molli1992@users.noreply.github.com>
2023-04-18 19:54:28 -0300

    Update logut.js

--- src/Components/Auth0/logut.js
+++ src/Components/Auth0/logut.js
@@ -14,8 +14,9 @@ export const Salir = () => {
     function eliminarCookies() {
         document.cookie.split(";").forEach(function (c) {
             document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-            logout();
-            window.location.href = "/"
+        });
+        logout().then((res) => {
+            window.location.href = "/";
         })
     };
 
